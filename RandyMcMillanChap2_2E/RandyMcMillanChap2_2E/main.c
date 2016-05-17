fddb4bad68e8bd6c116ea8e30e7abf2b9b864741
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-05-17 13:53:33 -0400

    Changes to be committed: 	new file:   ../RandyMcMillanChap2_2E.xcodeproj/project.pbxproj 	new file:   main.c

---
+++ RandyMcMillanChap2_2E/RandyMcMillanChap2_2E/main.c
@@ -0,0 +1,19 @@
+/*
+//  main.c
+//  RandyMcMillanChap2_2E
+//
+//  Created by randymcmillan on 5/17/16.
+//  Copyright © 2016 randymcmillan. All rights reserved.
+*/
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Hello, World!\n");
+    printf("new line\n");
+    printf("new line 2\n");
+    printf("new line 3\n");
+    printf("new line 4\n");
+    printf("new line 5\n");
+    return 0;
+}
