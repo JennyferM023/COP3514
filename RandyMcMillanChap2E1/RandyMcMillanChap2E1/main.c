aa2f7d55301f90a23be2288595b20df9593a44d2
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-05-17 13:28:04 -0400

    Changes to be committed: 	new file:   RandyMcMillanChap2E1/RandyMcMillanChap2E1.xcodeproj/project.pbxproj 	new file:   RandyMcMillanChap2E1/RandyMcMillanChap2E1/main.c 	new file:   index.html 	new file:   pun/index.html 	new file:   pun/pun.xcodeproj/index.html 	new file:   pun/pun/index.html 	new file:   syllabus.pdf

---
+++ RandyMcMillanChap2E1/RandyMcMillanChap2E1/main.c
@@ -0,0 +1,16 @@
+/*
+//  main.c
+//  RandyMcMillanChap2E1
+//
+//  Created by randymcmillan on 5/17/16.
+//  Copyright © 2016 randymcmillan. All rights reserved.
+*/
+
+#include <stdio.h>
+
+/* normally int main(void) { to NOT recieve arguments */
+int main(int argc, const char * argv[]) {
+    /* insert code here... */
+    printf("Hello, World!\n");
+    return 0;
+}
