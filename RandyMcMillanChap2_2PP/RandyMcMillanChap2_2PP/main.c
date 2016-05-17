9d37171251c837934a59dd785669d44f93b79a2d
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-05-17 15:14:20 -0400

    Changes to be committed: 	modified:   main.c

--- RandyMcMillanChap2_2PP/RandyMcMillanChap2_2PP/main.c
+++ RandyMcMillanChap2_2PP/RandyMcMillanChap2_2PP/main.c
@@ -8,26 +8,30 @@
 
 #include <stdio.h>
 
+    float volume;
+    int radius = 10;
+    int i;
+
 int main(int argc, const char * argv[]) {
 
-    float volume;
-    int radius;
-    radius = 10;
-     /* 
-      
-      Compute the volume with a 10 meter radius
-      
-      */
     
     int i;
     for (i=0;i < (sizeof (argv[i]));i++) {
         printf("print - %s\n",argv[i]);
         printf("argc - %d,\nargv[%s] - %s\n",argc,argv[i],argv[i]);
     }
+   
+    int output();
     
+    return 0;
+}
+
+int output(){
+
+
     volume = ((4.0f/3.0f) * (3.141592654) * radius * radius* radius);
     printf("The Volume of the Sphere with a %d meter radiusis %f \n",radius,volume);
     
-    
     return 0;
+    
 }
