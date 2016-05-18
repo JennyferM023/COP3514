760c000dcadfbf21b7643aa3d05f1b825b65f1d1
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-05-18 16:54:40 -0400

    Changes to be committed: 	modified:   randy_mcmillan_HW1.c

--- HW/randy_mcmillan_HW1.c
+++ HW/randy_mcmillan_HW1.c
@@ -10,60 +10,57 @@
 
 */
 
-#include <stdio.h>
-/*Declare some variables */
-    int a;
-    char slashChar[1] = "/";
-    char slash1;
-    char *slash11 = &slash1;
-    int b;
-    int c;
-    char slash2;
-    char *slash22 = &slash2;
-    int d;
-    int bool1 = 0;
-    int bool2 = 0;/*zero or not zero flagging */
-    int report();
-    int errorCheck();
-int main() {
-    /* Prompt user to input variable*/
-    printf("Enter a fraction (a/b): \n");
-    /* Receieve the user input*/
-    scanf( "%d%c%d", &a,&slash1,&b);
-    /* Prompt user to input variable*/
-    printf("Enter a fraction (c/d): \n");
-    /* Receieve the user input*/
-    scanf( "%d%c%d", &c,&slash2,&d);
+#include <stdio.h>/*preprocessor directive*/
+/*Declare some variables*/
+int a;
+const char slashChar[1] = "/";
+char slash1;
+char *slash11 = &slash1;
+int b;
+int c;
+char slash2;
+char *slash22 = &slash2;
+int d;
+int bool1 = 0;
+int bool2 = 0;/*zero or not zero flagging */
+int report();
+int errorCheck();
+  int main() {
+    a=b=c=d=0;
+      /* Prompt user to input variable*/
+      printf("Enter a fraction (a/b): \n");
+      /* Receieve the user input*/
+      scanf( "%d%c%d", &a,&slash1,&b);
+      /* Prompt user to input variable*/
+      printf("Enter a fraction (c/d): \n");
+      /* Receieve the user input*/
+      scanf( "%d%c%d", &c,&slash2,&d);
 
     report();
 
-    return 0;
-}
+  return 0;
+  }
 int report()
 {
-
     errorCheck();
-
     printf("first fraction  is %d/%d\n" ,a,b );
     printf("second fraction  is %d/%d\n",c,d );
 
-
-return 0;
+  return 0;
 }
 int errorCheck()
 {
 
-  printf("slashChar = %s\n",slashChar );
+  if ((slash11 != slashChar) || (slash22 != slashChar)){
 
 
-  if (slash1 < slash2 || slash1 > slash2) {
-    if (slash11 != slashChar || slash22 != slashChar) {
 
-    printf("Hmmm....\n");
 
-    }
-  }else{
-    printf("Some errorChecking here\n");
+printf("WTF\n");
+
   }
+
+
+
 return 0;
 }
