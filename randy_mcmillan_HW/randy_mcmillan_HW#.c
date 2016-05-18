002ef5cb17c3f7b73d0b8d882427497ae5afed73
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-05-18 15:17:09 -0400

    Changes to be committed: 	new file:   randy_mcmillan_HW/Homework 1.pdf 	new file:   randy_mcmillan_HW/randy_mcmillan_HW#.c 	new file:   randy_mcmillan_HW/randy_mcmillan_HW1.c

---
+++ randy_mcmillan_HW/randy_mcmillan_HW#.c
@@ -0,0 +1,17 @@
+/*
+
+  Randy McMillan
+
+  U8173-5382
+
+  Homework 1
+
+  COP 3514
+
+*/
+
+
+int main()
+{
+  return0;
+}
---
+++ randy_mcmillan_HW/randy_mcmillan_HW1.c
@@ -0,0 +1,30 @@
+/*
+
+  Randy McMillan
+
+  U8173-5382
+
+  Homework 1
+
+  COP 3514
+
+*/
+
+
+int main() {
+    char a, b;
+    printf("Enter a fraction (a/b): \n");
+    scanf( "%c", &a, &b);
+
+
+
+
+
+
+    printf("The sum of %d and %d is %d \n", a, b, a + b);
+    printf("The product of %d and %d is %d \n", a, b, a * b);
+    printf("The difference of %d and %d is %d \n", a, b, a - b);
+    printf("The quotient of %d and %d is %d \n", a, b, a / b);
+    printf("The remainder of %d and %d is %d \n", a, b, a % b);
+    return 0;
+}
