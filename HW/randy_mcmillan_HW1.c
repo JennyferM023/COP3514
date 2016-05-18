5956931c27f03d0afd6bfb7040ee398b12b6d1a7
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-05-18 17:44:45 -0400

    Changes to be committed: 	modified:   randy_mcmillan_HW1.c

--- HW/randy_mcmillan_HW1.c
+++ HW/randy_mcmillan_HW1.c
@@ -12,55 +12,83 @@
 
 #include <stdio.h>/*preprocessor directive*/
 /*Declare some variables*/
-int a;
 const char slashChar[1] = "/";
-char slash1;
-char *slash11 = &slash1;
+int a;
 int b;
 int c;
+int d;
+char slash1;
+char *slash11 = &slash1;
 char slash2;
 char *slash22 = &slash2;
-int d;
-int bool1 = 0;
-int bool2 = 0;/*zero or not zero flagging */
+char bool1;
+char bool2;/*zero or not zero flagging */
 int report();
 int errorCheck();
+int enterFirstFraction();
+int enterSecondFraction();
+int promptForRestart();
+
   int main() {
     a=b=c=d=0;
-      /* Prompt user to input variable*/
-      printf("Enter a fraction (a/b): \n");
-      /* Receieve the user input*/
-      scanf( "%d%c%d", &a,&slash1,&b);
-      /* Prompt user to input variable*/
-      printf("Enter a fraction (c/d): \n");
-      /* Receieve the user input*/
-      scanf( "%d%c%d", &c,&slash2,&d);
+    enterFirstFraction();
+    enterSecondFraction();
 
     report();
 
   return 0;
   }
+
+
+int enterFirstFraction(){
+
+      /* Prompt user to input variable*/
+      printf("Enter a fraction (a/b): \n");
+      /* Receieve the user input*/
+      scanf( "%d%c%d", &a,&slash1,&b);
+  return 0;
+}
+int enterSecondFraction(){
+
+      /* Prompt user to input variable*/
+      printf("Enter a fraction (c/d): \n");
+      /* Receieve the user input*/
+      scanf( "%d%c%d", &c,&slash2,&d);
+  return 0;
+}
 int report()
 {
     errorCheck();
     printf("first fraction  is %d/%d\n" ,a,b );
     printf("second fraction  is %d/%d\n",c,d );
-
   return 0;
 }
 int errorCheck()
 {
+printf("slash11 = %s \nslash22 = %s\n",slash11,slash22 );
 
   if ((slash11 != slashChar) || (slash22 != slashChar)){
 
+    printf("WTF\n");
+    printf("Please reenter fractions in in the form of ###/### \n");
 
-
-
-printf("WTF\n");
+    promptForRestart();
 
   }
 
+return 0;
+}
+int promptForRestart(){
 
-
+    printf("restart? y or n?\n");
+    scanf( "%s", &bool1);
+      switch (bool1) {
+        case 'y':
+          main();
+        case 'n':
+          break;
+        default:
+          break;
+      }
 return 0;
 }
