2521c22d3d7e55ba7784fa2538640a4f2b52981c
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-05-18 15:43:37 -0400

    Changes to be committed: 	modified:   randy_mcmillan_HW1.c

--- HW/randy_mcmillan_HW1.c
+++ HW/randy_mcmillan_HW1.c
@@ -12,15 +12,42 @@
 
 #include <stdio.h>
 
-int main() {
-    char a, slash, b;
-    char a2, slash2, b2;
-    printf("Enter a fraction (a/b): \n");
-    scanf( "%c%c%c", &a,&slash,&b);
-    printf("Enter a second fraction (a/b): \n");
-    scanf( "%c%c%c", &a2,&slash2,&b2);
+/*Declare some variables */
+    int a;
+    char slash;
+    int b;
+    int c;
+    char slash2;
+    int d;
 
-    printf("first fraction  is %c/%c\n",a,b );
+    int report();
+    int errorCheck();
+int main() {
+    /* Prompt user to input variable*/
+    printf("Enter a fraction (a/b): \n");
+    /* Receieve the user input*/
+    scanf( "%d%c%d", &a,&slash,&b);
+    /* Prompt user to input variable*/
+    printf("Enter a fraction (c/d): \n");
+    /* Receieve the user input*/
+    scanf( "%d%c%d", &c,&slash2,&d);
+
+    report();
+
+    return 0;
+}
+
+int errorCheck()
+{
+
+return 0;
+}
+int report()
+{
+
+
+    printf("first fraction  is %d/%d\n" ,a,b );
+    printf("second fraction  is %d/%d\n",c,d );
 
 
 
@@ -31,5 +58,6 @@ int main() {
     printf("The quotient of %d and %d is %d \n", a, b, a / b);
     printf("The remainder of %d and %d is %d \n", a, b, a % b);
 */
-    return 0;
+
+return 0;
 }
