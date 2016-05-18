3b7b9f0e75ff385ef7b2d0700dae2b5d26285ac4
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-05-18 18:41:20 -0400

    Changes to be committed: 	modified:   randy_mcmillan_HW1.c

--- HW/randy_mcmillan_HW1.c
+++ HW/randy_mcmillan_HW1.c
@@ -11,7 +11,7 @@
 */
 
 #include <stdio.h>/*preprocessor directive*/
-/*Declare some variables*/
+/*Declare some variables and pointers*/
 char slashchar[1] = "/";
 char *slashChar = &slashchar[0];
 int a;
@@ -25,11 +25,11 @@ char *slash22 = &slash2;
 char bool1;
 char bool2;
 /* Declare some functions*/
-int report();
-int enterFirstFraction();
-int enterSecondFraction();
+void report();
+void enterFirstFraction();
+void enterSecondFraction();
 int errorCheck();
-int promptForRestart();
+void promptForRestart();
 
   int main() {
     a=b=c=d=0;
@@ -38,45 +38,45 @@ int promptForRestart();
     report();
   return 0;
   }
-int enterFirstFraction(){
+void enterFirstFraction(){
       /* Prompt user to input variable*/
       printf("Enter a fraction (a/b): \n");
       /* Receieve the user input*/
       scanf( "%d%c%d", &a,&slash1,&b);
-  return 0;
 }
-int enterSecondFraction(){
+void enterSecondFraction(){
       /* Prompt user to input variable*/
       printf("Enter a fraction (c/d): \n");
       /* Receieve the user input*/
       scanf( "%d%c%d", &c,&slash2,&d);
-  return 0;
 }
-int report()
+void report()
 {
     errorCheck();
-    printf("first fraction  is %d/%d\n" ,a,b );
-    printf("second fraction  is %d/%d\n",c,d );
-  return 0;
+    //printf("first fraction  is %d/%d\n" ,a,b );
+    //printf("second fraction  is %d/%d\n",c,d );
 }
 int errorCheck()
 {
 
+/*
   printf("slash1 = %c \nslash2 = %c\n",slash1,slash2 );
   printf("*slash11 = %c \n*slash22 = %c\n",*slash11,*slash22 );
   printf("slashchar = %s \n*slashChar = %c\n",slashchar,*slashChar );
+*/
 
   if (slash1 != *slashChar){
     printf("Please reenter fractions in in the form of ###/### \n");
-    promptForRestart();
+    //promptForRestart();
+    return 1;
   }else if (slash2 != *slashChar){
     printf("Please reenter fractions in in the form of ###/### \n");
-    promptForRestart();
+    //promptForRestart();
+    return 1;
   }
-
 return 0;
 }
-int promptForRestart(){
+void promptForRestart(){
 
     printf("restart? y or n?\n");
     scanf( "%s", &bool1);
@@ -88,5 +88,4 @@ int promptForRestart(){
         default:
           break;
       }
-return 0;
 }
