fcdff82c990bfd0f02223ba426295b609d1a31fe
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-05-19 01:08:53 -0400

    Changes to be committed: 	renamed:    RandyMcMillanChap2E1/RandyMcMillanChap2E1.xcodeproj/project.pbxproj -> HW/RandyMcMillanChap2E1/RandyMcMillanChap2E1.xcodeproj/project.pbxproj 	renamed:    RandyMcMillanChap2E1/RandyMcMillanChap2E1/main.c -> HW/RandyMcMillanChap2E1/RandyMcMillanChap2E1/main.c 	renamed:    RandyMcMillanChap2_1PP/RandyMcMillanChap2_1PP.xcodeproj/project.pbxproj -> HW/RandyMcMillanChap2_1PP/RandyMcMillanChap2_1PP.xcodeproj/project.pbxproj 	renamed:    RandyMcMillanChap2_1PP/RandyMcMillanChap2_1PP/main.c -> HW/RandyMcMillanChap2_1PP/RandyMcMillanChap2_1PP/main.c 	renamed:    RandyMcMillanChap2_2E/RandyMcMillanChap2_2E.xcodeproj/project.pbxproj -> HW/RandyMcMillanChap2_2E/RandyMcMillanChap2_2E.xcodeproj/project.pbxproj 	renamed:    RandyMcMillanChap2_2E/RandyMcMillanChap2_2E/main.c -> HW/RandyMcMillanChap2_2E/RandyMcMillanChap2_2E/main.c 	renamed:    RandyMcMillanChap2_2PP/RandyMcMillanChap2_2PP.xcodeproj/project.pbxproj -> HW/RandyMcMillanChap2_2PP/RandyMcMillanChap2_2PP.xcodeproj/project.pbxproj 	renamed:    RandyMcMillanChap2_2PP/RandyMcMillanChap2_2PP/main.c -> HW/RandyMcMillanChap2_2PP/RandyMcMillanChap2_2PP/main.c 	renamed:    RandyMcMillanChap2_2PP/RandyMcMillanChap2_2PP/randy_mcmillan_HW0.c -> HW/RandyMcMillanChap2_2PP/RandyMcMillanChap2_2PP/randy_mcmillan_HW0.c 	renamed:    pun/index.html -> HW/pun/index.html 	renamed:    pun/pun.xcodeproj/index.html -> HW/pun/pun.xcodeproj/index.html 	renamed:    pun/pun.xcodeproj/project.pbxproj -> HW/pun/pun.xcodeproj/project.pbxproj 	renamed:    pun/pun/index.html -> HW/pun/pun/index.html 	renamed:    pun/pun/main.c -> HW/pun/pun/main.c 	renamed:    randy_mcmillan_HW/randy_mcmillan_HW#.c -> HW/randy_mcmillan_HW#.c 	new file:   HW/randy_mcmillan_HW1/Homework 1.pdf 	new file:   HW/randy_mcmillan_HW1/randy_mcmillan_HW1a.c 	renamed:    HW/randy_mcmillan_HW1.c -> HW/randy_mcmillan_HW1/randy_mcmillan_HW1b.c 	deleted:    randy_mcmillan_HW/Homework 1.pdf 	deleted:    randy_mcmillan_HW/randy_mcmillan_HW1.c

---
+++ HW/randy_mcmillan_HW1/randy_mcmillan_HW1a.c
@@ -0,0 +1,134 @@
+/*
+ *   Randy McMillan
+ *   U8173-5382
+ *   Homework 1
+ *   COP 3514
+ */
+
+#include <stdio.h>      /*preprocessor directive*/
+/*Declare some variables and pointers*/
+char slashchar[1]    = "/";
+char *slashChar              = &slashchar[0];
+int a;
+int b;
+int c;
+int d;
+char slash1;
+char *slash11 = &slash1;
+char slash2;
+char *slash22 = &slash2;
+char bool1;
+char bool2;
+int error1, error2;
+/*Declare some functions*/
+void myMain();
+
+void enterFirstFraction();
+
+void enterSecondFraction();
+
+int report();
+
+int errorCheckFraction1();
+
+int errorCheckFraction2();
+
+void promptForRestart();
+
+int main()
+{
+  a = b = c = d = 0;
+  myMain();
+  return 0;
+} /* main */
+
+void myMain()
+{
+  enterFirstFraction();
+  enterSecondFraction();
+  report();
+} /* myMain */
+
+void enterFirstFraction()
+{
+  /* Prompt user to input variable*/
+  printf("Enter the first fraction (a/b): \n");
+  /* Receieve the user input*/
+  scanf("%d%c%d", &a, &slash1, &b);
+} /* enterFirstFraction */
+
+void enterSecondFraction()
+{
+  /* Prompt user to input variable*/
+  printf("Enter the second fraction (c/d): \n");
+  /* Receieve the user input*/
+  scanf("%d%c%d", &c, &slash2, &d);
+} /* enterSecondFraction */
+
+int report()        /* begins an error checking loop */
+{
+  error1  = errorCheckFraction1();
+  error2  = errorCheckFraction2();
+
+  if ( (error1 == 0) && (error2 == 0) ) {
+     return 0;
+     }
+  else
+  if (error1 == 1)
+          {
+          printf("error1 = %d\n", error1);
+          enterFirstFraction();
+          report();
+          }
+  else
+  if (error2 == 1)
+          {
+          printf("error2 = %d\n", error2);
+          enterSecondFraction();
+          report();
+          }
+  else
+      {
+      promptForRestart();
+      }
+
+  return 1;             /*general error*/
+} /* report */
+
+int errorCheckFraction1()
+{
+  if (slash1 != * slashChar) {
+     printf("Please reenter the first fraction in in the form of ###/### \n");
+     return 1;
+     }
+
+  return 0;
+} /* errorCheckFraction1 */
+
+int errorCheckFraction2()
+{
+  if (slash2 != * slashChar) {
+     printf("Please reenter the second fraction in in the form of ###/### \n");
+     return 1;
+     }
+
+  return 0;
+} /* errorCheckFraction2 */
+
+void promptForRestart()
+{
+  printf("restart? y or n?\n");
+  scanf("%s", &bool1);
+
+  switch (bool1) {
+          case 'y':
+          main();
+
+          case 'n':
+          break;
+
+          default:
+          break;
+         }       /* switch */
+
+} /* promptForRestart */
