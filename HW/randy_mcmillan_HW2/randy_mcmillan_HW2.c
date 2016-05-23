da1eef2cdc133271d21e5e087063c13b3a5c0c39
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-05-23 01:54:48 -0400

    ..

---
+++ HW/randy_mcmillan_HW2/randy_mcmillan_HW2.c
@@ -0,0 +1,23 @@
+/*
+ *   Randy McMillan
+ *   U8173-5382
+ *   Homework 2
+ *   COP 3514
+ */
+#include <stdio.h> /*preprocessor directive*/
+/*Declare some variables and pointers*/
+/*Declare some functions*/
+void myMain();
+/*
+ *  Entry Point
+ */
+int main()
+{
+  myMain();
+  return 0;
+}   /* main */
+/*
+ *  myMain is where things really start
+ */
+void myMain()
+{}   /* myMain */
