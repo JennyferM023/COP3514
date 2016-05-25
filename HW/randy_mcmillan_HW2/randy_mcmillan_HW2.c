2473e2446c1875dbe7c501d6e088e1eb744c253b
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-05-25 16:20:03 -0400

    Changes to be committed: 	modified:   randy_mcmillan_HW2.c

--- HW/randy_mcmillan_HW2/randy_mcmillan_HW2.c
+++ HW/randy_mcmillan_HW2/randy_mcmillan_HW2.c
@@ -1,15 +1,15 @@
 /*
  *   Randy McMillan
  *   U8173-5382
- *   Homework 2
+ *   Homework 2 Magic Squares Excercise
  *   COP 3514
  */
-#include <stdio.h> /*preprocessor directive*/
+#include <stdio.h> /* preprocessor directive */
 /*Declare some variables, CONSTANTS , etc */
 #define MAX_SIZE 11 /* Generalizing for more dimensions */
 int  globalSide;
 int  globalArray[MAX_SIZE][MAX_SIZE]; /* prescribed MAX dimensions 11 x 11 */
-/*Declare some functions*/
+/* Declare some functions */
 void myMain();
 /*
  *  Entry Point
