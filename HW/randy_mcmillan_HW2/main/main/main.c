27ba300dd22d0c16a02e1a0978cbf1bd249f1079
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-05-27 17:33:33 -0400

    Changes to be committed: 	new file:   main/main.xcodeproj/project.pbxproj 	new file:   main/main/main.c 	modified:   randy_mcmillan_HW2b.c

---
+++ HW/randy_mcmillan_HW2/main/main/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  main
+//
+//  Created by randymcmillan on 5/27/16.
+//  Copyright © 2016 RandyMcMillan.net. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
--- HW/randy_mcmillan_HW2/randy_mcmillan_HW2b.c
+++ HW/randy_mcmillan_HW2/randy_mcmillan_HW2b.c
@@ -1,88 +1,35 @@
 /*
- *   Randy McMillan
- *   U8173-5382
- *   Homework 2 Magic Squares Excercise
- *   COP 3514
+ *
+ * Randy McMillan
+ *
+ * U8173-5382
+ *
+ * Homework 1
+ *
+ * COP 3514
+ *
+ */
+
+
+/*
+ * Store the magic square in a two dimensional array. Start by placing the number 1 in the middle of row 0. Place each of the remaining numbers 2, 3, ... , n2 by moving up one row and over one column. Any attempt to go outside the bounds of the array should "wrap around" to the opposite side of the array. For example, instead of storing the next number in row -1 (row negative 1), we would store it in row n - 1 (the last row). Instead of storing the next number in column n, we would store it in column 0. If a particular element is already occupied, put the number directly below the previously stored number.
+ * The magic square only needs to be up to 11 by 11 and can only be created from odd numbers.
+ * I want you to use functions for this project. I would like you to separately declare and define 3 separate functions. One for creating the magic square and putting it the two dimensional array. One for printing the magic square (for your printf, use 5 minimum field width). Finally, one for getting and returning the sum of the numbers in the magic square array.
+ * Take in input in the main function. If the input is an even number or is a number not between 1 and 11, exit the program. Also, print the value of the sum of magic numbers via the main function, since your function to calculate this should only be returning a integer.
+ * For the sum of all the numbers in the magic square array, I want you to use while loops. You can use either for loops or while loops for the print and create magic number functions.
+ * HINT: The only function that needs to return something is the sum magic square function (integer). The other two function don't need to return anything. C is a pass by value language, meaning that it typically can't change the values of arguments in functions. This doesn't apply to arrays because they are references (more in chapter 12). That means one argument in the functions should be a two dimensional array. You will be able to operate on this array.
  */
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 
-/* preprocessor directive */
-/* Declare some variables, CONSTANTS , etc */
-void  myMain();
-int   main()
-{
-  myMain();
+int main(
+  int         argc,
+  const char  *argv[]
+  ) {
+  /* Think monolithic... As in BASIC Programming 1991! */
+
+
+
+
+
   return 0;
-}   /* main */
-int i, j, k;
-
-void myMain()
-{
-
-int side, initPosX, initPosY, posX, posY, intValue, array[11][11];
-    memset( array, 0, sizeof(array) );
-
-  /* I want you to use functions for this project. I would like you to separately declare and define 3 separate functions. One for creating the magic square and putting it in the two dimensional array. */
-  memset( array, 0, sizeof(array) );
-  intValue = 1;
-
-  printf("Enter side length of magic square: " );
-  scanf("%d", &side );
-  initPosX = (side / 2);/* array starts with 0 */
-  initPosY = 0;
-  //initPosX = 0;/* array starts with 0 */
-  //initPosY = (side / 2);
-   printf(  "initPosX = %d, initPosY = %d \n",  initPosX,  initPosY);
-  posX = initPosX;
-  posY = initPosY;
-   printf(  "posX = %d, posY = %d\n",                  posX,      posY);
-   printf(  "intValue = %d\n\n\n",                         intValue );
-
-
-
-//start with 1
-  for (intValue = 1; intValue < side * side + 1; intValue ++)
-      {
-
-          printf( "array[%2d ][%2d ] = %d\n", posX, posY, array[posX][posY]);
-          //printf( "!array[%2d ][%2d ] = %d\n", posX, posY, !array[posX][posY]);
-
-      /* if 0 */
-      if (!array[posX][posY]) {
-      
-          
-          array[posX][posY]= intValue;
-          printf( "array[%2d ][%2d ] = %d\n\n\n", posX, posY, array[posX][posY]);
-          posX++,posY--;
-      
-      }
-      else{
-      
-          printf("not 0");
-          printf( "else array[%2d ][%2d ] = %d\n", posX, posY, array[posX][posY]);
-      
-      }
-
-      }
-
-//  int i, j;
-
-  for(j = 0; j < (side); j ++)
-      {
-      for(i = 0; i < (side); i ++)
-          {
-          //sleep(1);   // sleep for 1 seconds
-          printf(" %2d ", array[i][j]);
-          //sleep(1); // sleep for 1 seconds
-
-          }
-
-      printf("\n");
-      }
-
-
-}   /* myMain */
-/* One for printing the magic square (for your printf, use 5 minimum field width). Finally, one for getting and returning the sum of the numbers in the magic square array. */
+} /* main */
