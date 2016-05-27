77c68c50f16628430ae7d0f5613c3efa06fb552e
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-05-27 09:37:35 -0400

    basic idea - need to refine  Changes to be committed: 	modified:   randy_mcmillan_HW2b.c

--- HW/randy_mcmillan_HW2/randy_mcmillan_HW2b.c
+++ HW/randy_mcmillan_HW2/randy_mcmillan_HW2b.c
@@ -15,6 +15,8 @@ int   main()
   myMain();
   return 0;
 }   /* main */
+int i, j;
+
 void myMain()
 {
 
@@ -33,61 +35,45 @@ int side, initPosX, initPosY, posX, posY, intValue, array[11][11];
    printf(  "posX = %d, posY = %d\n",                  posX,      posY);
    printf(  "intValue = %d\n",                         intValue );
 
-//
+//set square 1 center x,  0 y
   array[posX][posY] = intValue;
    printf( "array[%d][%d] = %d\n", posX, posY, array[posX][posY]);
-  posX ++;
-  posY --;
 
+//start with 2
   for (intValue = 2; intValue < side * side; intValue ++)
       {
-
-      /* populate square */
-      if (posX >= side || posY <= -1 ) {
-         printf("boundary condition posX = %d posY = %d\n", posX, posY );
-
-         if (posX == side && posY == -1)
-            {
-            posX = 0;
-            posY = side - 1;
-            /* move to bottom left corner */
-            array[posX][posY] = intValue;
-            printf( "array[%d][%d] = %d\n", posX, posY, array[posX][posY]);
-            //break;
-            }
-
-         if (posY == -1 && posX != side)
-            {
-            posY = side - 1;/*adjusts for array index */
-            array[posX][posY] = intValue;
-            printf( "array[%d][%d] = %d\n", posX, posY, array[posX][posY]);
-            //break;
-            }
-
-         if (posX == side && posY != -1)
-            {
-            posX = 0;/* move to first column */
-            array[posX][posY] = intValue;
-            printf( "array[%d][%d] = %d\n", posX, posY, array[posX][posY]);
-            //break;
-            }
-
-         }
-      else
-          {
-
-           printf(  "non boundary cond. posX=%d posY=%d\n",  posX,  posY );
-          //printf(  "array[%d][%d] = %d\n",                  posX,  posY,  array[posX][posY]);
-          array[posX][posY] = intValue;
-           printf(  "array[%d][%d] = %d\n",                  posX,  posY, array[posX][posY]);
-          //posX ++;
-          //posY --;
-          //break;
-          }
-
       posX ++;
       posY --;
+
+      //boundary conditions
+      if ( posX > (side - 0) ) { posX = 0; }
+
+      if ( posY < 0 ) { posY = side - 1; }
+
+      array[posX][posY] = intValue;
+
+       printf(  "posX = %d, posY = %d\n",  posX,  posY);
+       printf(  "intValue = %d\n",         intValue );
+       printf(  "array[%d][%d] = %d\n",    posX,  posY, array[posX][posY]);
+
+      /* populate square */
+
+
+
       }
+
+//  int i, j;
+
+  for(i = 0; i < (side); i ++)
+      {
+      for(j = 0; j < (side); j ++)
+          {
+          printf("%d ", array[i][j]);
+          }
+
+      printf("\n");
+      }
+
 
 }   /* myMain */
 /* One for printing the magic square (for your printf, use 5 minimum field width). Finally, one for getting and returning the sum of the numbers in the magic square array. */
