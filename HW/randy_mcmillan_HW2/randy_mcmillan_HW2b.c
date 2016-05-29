b4a7e901e8ff49fb1475034df43b1c8a900d4cdb
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-05-29 11:46:40 -0400

    Changes to be committed: 	modified:   HW/randy_mcmillan_HW2/randy_mcmillan_HW2b.c

--- HW/randy_mcmillan_HW2/randy_mcmillan_HW2b.c
+++ HW/randy_mcmillan_HW2/randy_mcmillan_HW2b.c
@@ -4,21 +4,12 @@
  *
  * U8173-5382
  *
- * Homework 1
+ * Homework 2
  *
  * COP 3514
  *
  */
 
-
-/*
- * Store the magic square in a two dimensional array. Start by placing the number 1 in the middle of row 0. Place each of the remaining numbers 2, 3, ... , n2 by moving up one row and over one column. Any attempt to go outside the bounds of the array should "wrap around" to the opposite side of the array. For example, instead of storing the next number in row -1 (row negative 1), we would store it in row n - 1 (the last row). Instead of storing the next number in column n, we would store it in column 0. If a particular element is already occupied, put the number directly below the previously stored number.
- * The magic square only needs to be up to 11 by 11 and can only be created from odd numbers.
- * I want you to use functions for this project. I would like you to separately declare and define 3 separate functions. One for creating the magic square and putting it the two dimensional array. One for printing the magic square (for your printf, use 5 minimum field width). Finally, one for getting and returning the sum of the numbers in the magic square array.
- * Take in input in the main function. If the input is an even number or is a number not between 1 and 11, exit the program. Also, print the value of the sum of magic numbers via the main function, since your function to calculate this should only be returning a integer.
- * For the sum of all the numbers in the magic square array, I want you to use while loops. You can use either for loops or while loops for the print and create magic number functions.
- * HINT: The only function that needs to return something is the sum magic square function (integer). The other two function don't need to return anything. C is a pass by value language, meaning that it typically can't change the values of arguments in functions. This doesn't apply to arrays because they are references (more in chapter 12). That means one argument in the functions should be a two dimensional array. You will be able to operate on this array.
- */
 #include <stdio.h>
 #define MAX_SIDELENGTH 11
 /* Store the magic square in a two dimensional array. */
@@ -27,10 +18,9 @@ int magicArray[MAX_SIDELENGTH][MAX_SIDELENGTH];
 void  outputMagicSquare(int array[MAX_SIDELENGTH][MAX_SIDELENGTH]);
 int   sumOfTheBox(int array[MAX_SIDELENGTH][MAX_SIDELENGTH]);
 int   globalSum;
-/**/
-int  iHat, jHat, sideSquared;
-int  globalSideLength;
-int  yPos, xPos;
+int   iHat, jHat, sideSquared;
+int   globalSideLength;
+int   yPos, xPos;
 
 int main(
   int         argc,
@@ -48,9 +38,9 @@ int main(
      return 0;/* stops program if the side length is even or max side > max */
      }
 
+  /* Start by placing the number 1 in the middle of row 0. */
   yPos = 0;
   xPos = globalSideLength / 2;
-  /* Start by placing the number 1 in the middle of row 0. */
   sideSquared = globalSideLength * globalSideLength;
 
 /* Place each of the remaining numbers 2, 3, ... , n2 by moving up one row and over one column. */
@@ -59,7 +49,6 @@ int main(
 
       magicArray[xPos][yPos] = iHat;
 
-      /* printf("%d\n", magicArray[xPos][yPos] ); */
       /* Any attempt to go outside the bounds of the array
        * should "wrap around" to the opposite side of the array.
        */
@@ -102,7 +91,7 @@ int main(
       }
 
   outputMagicSquare(magicArray);
-  printf( "Sum of Magic Numbers: %d\n", sumOfTheBox(magicArray) );
+  printf( "Sum of Magic Numbers: %d\n", sumOfTheBox(magicArray) ); /* return type int from sumOfBox function */
   return 0;
 } /* main */
 void outputMagicSquare(int array[MAX_SIDELENGTH][MAX_SIDELENGTH]) {
@@ -128,19 +117,16 @@ void outputMagicSquare(int array[MAX_SIDELENGTH][MAX_SIDELENGTH]) {
  * create magic number functions.
  * HINT: The only function that needs to return something is the sum magic
  * square function (integer). The other two function don't need to return
- * anything. C is a pass by value language, meaning
- * that it typically can't change the values of arguments in functions. This
- * doesn't apply to arrays because they are references (more in chapter 12).
- * That means one argument in the functions
- * should be a two dimensional array. You will be able to operate on this array.
+ * anything.
  */
 int sumOfTheBox(int array[MAX_SIDELENGTH][MAX_SIDELENGTH]){
 
-int  sum = 0;
+int  sum;
 int  iHat;
 int  jHat;
 
   iHat = 0;
+  sum  = 0;
 
 /* while loop */
   while (iHat < globalSideLength) {
@@ -149,10 +135,10 @@ int  jHat;
 
         while (jHat < globalSideLength) {
               sum = sum + magicArray[iHat][jHat];
-              jHat ++;
+              jHat ++; /* increment */
               }
 
-        iHat ++;
+        iHat ++; /* increment */
 
         }
 
