fa9333861a52b1e792b876e9ff48dbc0ad0dbe83
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-06-01 13:33:00 -0400

    Changes to be committed: 	new file:   HW/randy_mcmillan_HW3/Notes.txt 	new file:   HW/randy_mcmillan_HW3/poker.c

---
+++ HW/randy_mcmillan_HW3/poker.c
@@ -0,0 +1,24 @@
+/*
+ *
+ * Randy McMillan
+ *
+ * U8173-5382
+ *
+ * Homework 1
+ *
+ * COP 3514
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_RANKS 13
+#defime numINRanks[...]
+
+int main(
+  int         argc,
+  const char  *argv[]
+  )
+{
+  return 0;
+} /* main */
