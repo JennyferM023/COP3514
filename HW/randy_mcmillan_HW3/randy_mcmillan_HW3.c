d67c58ce9c9c4a40840dc011b137d9676b33379e
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-06-08 22:36:54 -0400

    Changes to be committed: 	new file:   Homework3.pdf 	new file:   hw3/Homework3.pdf 	modified:   hw3/hw3.xcodeproj/project.pbxproj 	modified:   randy_mcmillan_HW3.c

--- HW/randy_mcmillan_HW3/randy_mcmillan_HW3.c
+++ HW/randy_mcmillan_HW3/randy_mcmillan_HW3.c
@@ -4,12 +4,28 @@
  *
  * U8173-5382
  *
- * Homework 1
+ * Homework 3
  *
  * COP 3514
  *
+ * All of your printing and scanning should be done in main. You are scanning in
+ * integers, but you should be holding values in floats because precision is
+ * important. When printing the roots, there are three cases. You will have two
+ * roots and will print two roots. You will have one root and will print that
+ * one. Or you will have no real roots (probably resulting in some weird data in
+ * your variables) and will just print "No real roots". You will be using
+ * pointers to pass around to functions to hold values that are calculated.
+ * Print all floats to 2 decimal places. Scan all of your coefficients on one
+ * line separated by spaces.
+ *
+ *
  */
 #include <stdio.h>
+
+int   a, b, c;
+int   x, y;
+void  quadraticFormula(), quadraticVertex(), quadraticInfo();
+
 
 int main(
   int         argc,
@@ -17,9 +33,47 @@ int main(
   )
 {
 
-
-
-
-
   return 0;
 } /* main */
+void quadraticFormula()  {
+  /*
+   * The first function will preform the quadratic equation to find the roots of
+   * the expression. The return value of the function will be an int that
+   * signifies how many REAL roots there are. You do not need to worry about
+   * imaginary numbers.
+   */
+} /* quadraticFormula */
+void quadraticVertex()  {
+  /*
+   * The second function is used to find the vertex of the expression. There is
+   * no return value for this function. Remember, there is an x and a y
+   * component to vertex.
+   */
+} /* quadraticVertex */
+void quadraticInfo() {
+  /*
+   * The third function function is to find the directrix and the direction the
+   * graph of the function is facing. There is no return value for this
+   * function. The direction of the graph will be represented by a char that
+   * will either be 'U' for up or 'D' for down.
+   */
+} /* quadraticInfo */
+/*
+ * Sample Output:
+ * Enter coefficents a, b, and c: 3 18 -48 f(x) = 3x^2 + 18x + -48
+ * Roots: 2.00 and -8.00
+ * Vertex: (-3.00, -75.00)
+ * Directrix: y = -75.08
+ * The graph is facing up
+ * Enter coefficents a, b, and c: 2 24 72 f(x) = 2x^2 + 24x + 72
+ * Root: -6.00
+ * Vertex: (-6.00, 0.00)
+ * Directrix: y = -0.12
+ * The graph is facing up
+ * Enter coefficents a, b, and c: 1 4 5 f(x) = 1x^2 + 4x + 5
+ * No real roots
+ * Vertex: (-2.00, 1.00)
+ * Directrix: y = 0.75
+ * Enter coefficents a, b, and c: -1 4 5 f(x) = -1x^2 + 4x + 5
+ * Roots: -1.00
+ */
