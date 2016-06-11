84f8aabdcf2706587e02c2efe028c038ce94baca
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-06-11 19:32:22 -0400

    Changes to be committed: 	modified:   .gitignore 	modified:   HW/RandyMcMillanChap2E1/RandyMcMillanChap2E1.xcodeproj/index.html 	modified:   HW/RandyMcMillanChap2E1/RandyMcMillanChap2E1/index.html 	modified:   HW/RandyMcMillanChap2E1/index.html 	modified:   HW/RandyMcMillanChap2_1PP/RandyMcMillanChap2_1PP.xcodeproj/index.html 	modified:   HW/RandyMcMillanChap2_1PP/RandyMcMillanChap2_1PP/index.html 	modified:   HW/RandyMcMillanChap2_1PP/index.html 	modified:   HW/RandyMcMillanChap2_2E/RandyMcMillanChap2_2E.xcodeproj/index.html 	modified:   HW/RandyMcMillanChap2_2E/RandyMcMillanChap2_2E/index.html 	modified:   HW/RandyMcMillanChap2_2E/index.html 	modified:   HW/RandyMcMillanChap2_2PP/RandyMcMillanChap2_2PP.xcodeproj/index.html 	modified:   HW/RandyMcMillanChap2_2PP/RandyMcMillanChap2_2PP/index.html 	modified:   HW/RandyMcMillanChap2_2PP/index.html 	modified:   HW/index.html 	new file:   HW/pointerPractice/index.html 	new file:   HW/pointerPractice/pointerPractice/index.html 	modified:   HW/pun/index.html 	modified:   HW/pun/pun.xcodeproj/index.html 	modified:   HW/pun/pun/index.html 	modified:   HW/randy_mcmillan_HW1/index.html 	modified:   HW/randy_mcmillan_HW10/index.html 	modified:   HW/randy_mcmillan_HW2/hw2/hw2.xcodeproj/index.html 	modified:   HW/randy_mcmillan_HW2/hw2/hw2/index.html 	modified:   HW/randy_mcmillan_HW2/hw2/index.html 	modified:   HW/randy_mcmillan_HW2/index.html 	modified:   HW/randy_mcmillan_HW3/hw3/hw3.xcodeproj/index.html 	modified:   HW/randy_mcmillan_HW3/hw3/hw3/index.html 	modified:   HW/randy_mcmillan_HW3/hw3/index.html 	modified:   HW/randy_mcmillan_HW3/index.html 	modified:   HW/randy_mcmillan_HW4/hw4/hw4.xcodeproj/index.html 	modified:   HW/randy_mcmillan_HW4/hw4/hw4/index.html 	modified:   HW/randy_mcmillan_HW4/hw4/index.html 	modified:   HW/randy_mcmillan_HW4/index.html 	modified:   HW/randy_mcmillan_HW5/index.html 	modified:   HW/randy_mcmillan_HW6/index.html 	modified:   HW/randy_mcmillan_HW7/index.html 	modified:   HW/randy_mcmillan_HW8/index.html 	modified:   HW/randy_mcmillan_HW9/index.html 	modified:   ProjectAssignment/complex.h/complex.h/complex.h.c 	new file:   ProjectAssignment/complex.h/complex.h/index.html 	new file:   ProjectAssignment/complex.h/index.html 	new file:   ProjectAssignment/index.html 	modified:   Quizzes/Quiz1/index.html 	modified:   Quizzes/Quiz2/index.html 	modified:   Quizzes/index.html 	modified:   ReferenceMaterial/index.html 	modified:   TextBook/AnswersToSelectedExcercises/index.html 	modified:   TextBook/index.html 	modified:   TextBook/knking.com/books/c2/answers/index.html 	modified:   TextBook/knking.com/books/c2/index.html 	modified:   TextBook/knking.com/books/c2/programs/index.html 	modified:   TextBook/knking.com/books/index.html 	modified:   TextBook/knking.com/books/java/index.html 	modified:   TextBook/knking.com/books/java/jpb/index.html 	modified:   TextBook/knking.com/books/modula2/index.html 	modified:   TextBook/knking.com/courses/index.html 	modified:   TextBook/knking.com/index.html 	modified:   TextBook/knking.com/links/index.html 	modified:   TextBook/knking.com/recbooks/index.html 	modified:   TextBook/knking.com/recbooks/java/index.html 	modified:   TextBook/the_c_programming_language_2/index.html 	modified:   index.html

--- ProjectAssignment/complex.h/complex.h/complex.h.c
+++ ProjectAssignment/complex.h/complex.h/complex.h.c
@@ -7,13 +7,73 @@
  */
 
 #include <stdio.h>
+#include <math.h>
 #include <complex.h>
+
+void cargDemo (
+  double complex);
+void ccoszDemo (
+  double  a,
+  double  b
+  );
+
 int main(
   int         argc,
   const char  *argv[]
   ) {
 
-
+int             a, b;
+double complex  z = -4.4 + 3.3 * I;
+  cargDemo(z);
+  ccoszDemo(a, b);
 
   return 0;
 } /* main */
+void cargDemo(
+  double complex z){
+
+double  x = creal(z);
+double  y = cimag(z);
+
+double  radius = cabs(z);
+double  argument = carg(z);
+
+   printf( "cartesian(x, y): (%4.1f, %4.1f)\n",  x,       y);
+   printf( "polar(r, theta): (%4.1f, %4.1f)\n",  radius,  argument);
+
+
+} /* cargDemo */
+void ccoszDemo(
+  double  a,
+  double  b
+  ){
+
+
+double ccosz = ( exp (a * b) + exp (-a * b) ) / 2;
+  printf("ccosz = %f\n", ccosz);
+
+
+
+} /* ccoszDemo */
+//ccos(z) = (exp(i * z) + exp(-i * z)) / 2
+
+//casinh(z) = clog(z + csqrt(z * z + 1))
+
+//double complex casinh(double complex x);
+
+
+
+/*
+ * Cpow
+ *
+ * int x, y, p, q;
+ * double complex z1, z2, z3; //complex variables declaration
+ * scanf("%d %d %d %d", &x, &y, &p, &q);
+ * z1 = (p + q * I);
+ * z2 = (x + y * I);
+ * z3 = cpow (z2, z1);
+ * printf("%f + %f * i\n", creal(z3), cimag(z3));
+ *
+ *
+ *
+ */
