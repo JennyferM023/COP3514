be0d35b852c927d23d350ec31d7173d9bb549d98
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-06-15 17:17:01 -0400

    Changes to be committed: 	modified:   HW/randy_mcmillan_HW4/randy_mcmillan_HW4.c

--- HW/randy_mcmillan_HW4/randy_mcmillan_HW4.c
+++ HW/randy_mcmillan_HW4/randy_mcmillan_HW4.c
@@ -16,63 +16,29 @@
  * reflectTriangle translateTriangle, and printTriangle.
  */
 
-/*
- * The clockwiseRotateTriangle will rotate the triangle 90 degrees clockwise.
- * You shouldn't need the math library to complete this. The function will
- * return nothing. It will have arguments of an input array that holds the
- * points (the original triangle you had the user input) and an array to place
- * the new rotated triangle in.
- */
 void clockwiseRotateTriangle (
   int  points,
   int  newPoints
   );
 
-/*
- * The reflectTriangle will reflect the triangle over the x or y axis. You will
- * have the user input a axis via scanf in the form of a character (Do the
- * scanning in main). The function will return an integer, 0 or -1. 0 if the
- * axis was valid (x or y or X or Y) and -1 if it was invalid. It will have
- * arguments of an input array that holds the points (the original triangle you
- * had the user input), an array to place the new reflect triangle in, and a
- * character to denote the axis to reflect over.
- */
 int reflectTriangle (
   int  points,
   int  newPoints
   );
 
-/*
- * The translateTriangle will translate the triangle an x and/or y direction.
- * You will have the user input a two values via scanf in the form of a integers
- * (Do the scanning in main) to pass to the function for the translation. The
- * function will return nothing. It will have arguments of an input array that
- * holds the points (the original triangle you had the user input), an array to
- * place the new translated triangle in,an integer to translate in the x
- * direction, and an integer to translate in y direction.
- */
 void translateTriangle (
   int  x,
   int  y
   );
 
-/*
- * Finally, you will have a printTriangle function for printing the triangle on
- * the screen. This print triangle function will take in an input array that
- * holds the points of a triangle to print. You will then loop through the array
- * to print the triangle. HOWEVER, I want you to iterate through the triangle
- * using pointer arithmetic (examples in chapter 12).
- */
-void printTriangle ();
-
-
+void printTriangle (
+  int points[]);
 
 int main(
   int         argc,
   const char  *argv[]
   )
 {
-
   /*
    * This program is a good example of using arrays as points and reiterates
    * using arrays as paramaters for functions. Only do your scanning in main.
@@ -84,18 +50,58 @@ int main(
 void clockwiseRotateTriangle (
   int  points,
   int  newPoints
-  ){}
-
+  ){
+  /*
+   * The clockwiseRotateTriangle will rotate the triangle 90 degrees clockwise.
+   * You shouldn't need the math library to complete this. The function will
+   * return nothing. It will have arguments of an input array that holds the
+   * points (the original triangle you had the user input) and an array to place
+   * the new rotated triangle in.
+   */
+} /* clockwiseRotateTriangle */
 int reflectTriangle (
   int  points,
   int  newPoints
-  ){}
-
+  ){
+  /*
+   * The reflectTriangle will reflect the triangle over the x or y axis.
+   * You will
+   * have the user input a axis via scanf in the form of a character (Do the
+   * scanning in main). The function will return an integer, 0 or -1. 0 if the
+   * axis was valid (x or y or X or Y) and -1 if it was invalid. It will have
+   * arguments of an input array that holds the points
+   * (the original triangle you
+   * had the user input), an array to place the new reflect triangle in, and a
+   * character to denote the axis to reflect over.
+   */
+} /* reflectTriangle */
 void translateTriangle (
   int  x,
   int  y
-  ){}
-
+  ){
+  /*
+   * The translateTriangle will translate the triangle an x and/or y direction.
+   * You will have the user input a two values via scanf in the form of a
+   * integers
+   * (Do the scanning in main) to pass to the function for the translation. The
+   * function will return nothing. It will have arguments of an input array that
+   * holds the points (the original triangle you had the user input), an array
+   * to
+   * place the new translated triangle in,an integer to translate in the x
+   * direction, and an integer to translate in y direction.
+   */
+} /* translateTriangle */
+void printTriangle (
+  int points[]) {
+  /*
+   * Finally, you will have a printTriangle function for printing the
+   * triangle on
+   * the screen. This print triangle function will take in an input array that
+   * holds the points of a triangle to print. You will then loop through the array
+   * to print the triangle. HOWEVER, I want you to iterate through the triangle
+   * using pointer arithmetic (examples in chapter 12).
+   */
+} /* printTriangle */
 /*
  *
  * Enter point #1 as x and y: 2 7
